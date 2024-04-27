b92895d713d6b2947985994d185e219deb95405f
Jonas Lindmark <jonas@denacode.se>
2024-04-27 07:28:36 +0200

    Refactor wifi to separate module

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@
 
 use core::str::FromStr;
 
-use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_net::{Config, Ipv4Address, Stack, StackResources};
+use embassy_net::Ipv4Address;
 use embassy_net::tcp::TcpSocket;
 use embassy_rp::{bind_interrupts, pio};
-use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::pio::Pio;
+use embassy_rp::peripherals::PIO0;
 use embassy_time::{Duration, Timer};
 use rust_mqtt::client::client::MqttClient;
 use rust_mqtt::client::client_config::{ClientConfig, MqttVersion};
@@ -24,9 +21,12 @@ use rust_mqtt::packet::v5::reason_codes::ReasonCode;
 use rust_mqtt::utils::rng_generator::CountingRng;
 use serde::Serialize;
 use serde_json_core::to_string;
-use static_cell::StaticCell;
 
 use {defmt_rtt as _, panic_probe as _};
+
+use crate::wifi::init_wifi;
+
+mod wifi;
 
 const WIFI_NETWORK: &str = env!("WIFI_NETWORK");
 const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
@@ -39,16 +39,6 @@ bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => pio::InterruptHandler<PIO0>;
 });
 
-#[embassy_executor::task]
-async fn wifi_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
-    runner.run().await
-}
-
-#[embassy_executor::task]
-async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
-    stack.run().await
-}
-
 #[derive(Debug, Serialize)]
 struct Payload {
     data: u32,
@@ -58,72 +48,8 @@ struct Payload {
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
-    // To include cyw43 firmware in build (for uf2 builds)
-    //let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
-    //let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+    let (mut control, stack) = init_wifi(spawner, p).await.unwrap();
 
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download cyw43-firmware/43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs download cyw43-firmware/43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
-    let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-    let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
-
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
-
-    static STATE: StaticCell<cyw43::State> = StaticCell::new();
-    let state = STATE.init(cyw43::State::new());
-    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-    unwrap!(spawner.spawn(wifi_task(runner)));
-
-    control.init(clm).await;
-    control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
-
-    let config = Config::dhcpv4(Default::default());
-    // Generate random seed
-    let seed = 0x0123_4567_89ab_cdef; // chosen by fair dice roll. guarenteed to be random.
-
-    // Init network stack
-    static STACK: StaticCell<Stack<cyw43::NetDriver<'static>>> = StaticCell::new();
-    static RESOURCES: StaticCell<StackResources<2>> = StaticCell::new();
-    let stack = &*STACK.init(Stack::new(
-        net_device,
-        config,
-        RESOURCES.init(StackResources::<2>::new()),
-        seed,
-    ));
-
-    unwrap!(spawner.spawn(net_task(stack)));
-
-    loop {
-        match control.join_wpa2(WIFI_NETWORK, WIFI_PASSWORD).await {
-            Ok(_) => break,
-            Err(err) => {
-                info!("join failed with status={}", err.status);
-            }
-        }
-    }
-
-    // Wait for DHCP, not necessary when using static IP
-    info!("waiting for DHCP...");
-    while !stack.is_config_up() {
-        Timer::after_millis(100).await;
-    }
-    info!("DHCP is now up!");
-
-    info!("Waiting to get IP address...");
-    loop {
-        if let Some(config) = stack.config_v4() {
-            info!("Got IP: {}", config.address); //dhcp IP address
-            break;
-        }
-        Timer::after(Duration::from_millis(500)).await;
-    }
 
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
---
+++ src/wifi.rs
@@ -0,0 +1,93 @@
+use cyw43::{Control, NetDriver};
+use cyw43_pio::PioSpi;
+use defmt::{info, unwrap};
+use embassy_executor::Spawner;
+use embassy_net::{Config, Stack, StackResources};
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::Peripherals;
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::Pio;
+use embassy_time::{Duration, Timer};
+use heapless::String;
+use static_cell::StaticCell;
+use crate::{Irqs, WIFI_NETWORK, WIFI_PASSWORD};
+
+#[embassy_executor::task]
+async fn wifi_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
+    stack.run().await
+}
+
+pub async fn init_wifi(spawner: Spawner, p: Peripherals) -> Result<(Control<'static>, &'static Stack<NetDriver<'static>>), String<256>> {
+    // To include cyw43 firmware in build (for uf2 builds)
+    //let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
+    //let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs download cyw43-firmware/43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs download cyw43-firmware/43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
+    let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
+    let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let config = Config::dhcpv4(Default::default());
+    // Generate random seed
+    let seed = 0x0123_4567_89ab_cdef; // chosen by fair dice roll. guarenteed to be random.
+
+    // Init network stack
+    static STACK: StaticCell<Stack<cyw43::NetDriver<'static>>> = StaticCell::new();
+    static RESOURCES: StaticCell<StackResources<2>> = StaticCell::new();
+    let stack = &*STACK.init(Stack::new(
+        net_device,
+        config,
+        RESOURCES.init(StackResources::<2>::new()),
+        seed,
+    ));
+
+    unwrap!(spawner.spawn(net_task(stack)));
+
+    loop {
+        match control.join_wpa2(WIFI_NETWORK, WIFI_PASSWORD).await {
+            Ok(_) => break,
+            Err(err) => {
+                info!("join failed with status={}", err.status);
+            }
+        }
+    }
+
+    // Wait for DHCP, not necessary when using static IP
+    info!("waiting for DHCP...");
+    while !stack.is_config_up() {
+        Timer::after_millis(100).await;
+    }
+    info!("DHCP is now up!");
+
+    info!("Waiting to get IP address...");
+    loop {
+        if let Some(config) = stack.config_v4() {
+            info!("Got IP: {}", config.address); //dhcp IP address
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+    Ok((control, stack))
+}
