42f27102a4adfa9cd721c510caa0f17ff630ffca
Jonas Lindmark <jonas@denacode.se>
2024-07-10 09:32:40 +0200

    Implement serialization for readouts

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,14 @@ use embassy_rp::peripherals::{PIO0, UART1};
 use embassy_time::{Duration, Timer};
 use serde::Serialize;
 use serde_json_core::to_string;
+use time::OffsetDateTime;
 
 use crate::mqtt::{init_mqtt_client, send_message};
+use crate::serial::{init_serial, read_telegram, SerialPeripherals};
 use crate::wifi::{init_wifi, WifiPeripherals};
 
+
 use {defmt_rtt as _, panic_probe as _};
-use crate::serial::{init_serial, SerialPeripherals};
 
 mod wifi;
 mod mqtt;
@@ -28,9 +30,68 @@ bind_interrupts!(struct Irqs {
     UART1_IRQ => uart::BufferedInterruptHandler<UART1>;
 });
 
+#[derive(Debug, Serialize, Default)]
+struct EnergyPayload {
+    active_wh: u32,
+    reactive_varh: u32,
+}
+
+#[derive(Debug, Serialize, Default)]
+struct PowerPayload {
+    active_w: u32,
+    reactive_var: u32,
+}
+
+#[derive(Debug, Serialize, Default)]
+struct EnergyWithDirection {
+    from_grid: EnergyPayload,
+    to_grid: EnergyPayload,
+}
+
+#[derive(Debug, Serialize, Default)]
+struct PowerWithDirection {
+    from_grid: PowerPayload,
+    to_grid: PowerPayload,
+}
+
 #[derive(Debug, Serialize)]
 struct Payload {
-    data: u32,
+    time: OffsetDateTime,
+
+    energy: EnergyWithDirection,
+
+    total_power: PowerWithDirection,
+
+    power_l1: PowerWithDirection,
+    power_l2: PowerWithDirection,
+    power_l3: PowerWithDirection,
+
+    voltage_l1_dv: u16,
+    voltage_l2_dv: u16,
+    voltage_l3_dv: u16,
+
+    current_l1_da: u16,
+    current_l2_da: u16,
+    current_l3_da: u16,
+}
+
+impl Default for Payload {
+    fn default() -> Payload {
+        Payload {
+            time: OffsetDateTime::from_unix_timestamp(0).unwrap(),
+            energy: Default::default(),
+            total_power: Default::default(),
+            power_l1: Default::default(),
+            power_l2: Default::default(),
+            power_l3: Default::default(),
+            voltage_l1_dv: 0,
+            voltage_l2_dv: 0,
+            voltage_l3_dv: 0,
+            current_l1_da: 0,
+            current_l2_da: 0,
+            current_l3_da: 0,
+        }
+    }
 }
 
 
@@ -42,31 +103,26 @@ async fn main(spawner: Spawner) {
         WifiPeripherals::new(p.PIN_23, p.PIN_25, p.PIN_24, p.PIN_29, p.PIO0, p.DMA_CH0);
     let (mut control, stack) = init_wifi(spawner, wp).await;
 
-    let sp = SerialPeripherals::new(p.PIN_9, p.UART1);
-    init_serial(spawner, sp).await;
-
+    info!("Starting main loop");
     loop {
-        let mut client = match init_mqtt_client(stack).await {
+        let client = match init_mqtt_client(stack).await {
             Ok(c) => c,
             Err(()) => continue
         };
 
-        let mut count: u32 = 0;
+        let sp = SerialPeripherals::new(p.PIN_9, p.UART1);
+        let mut han_reader = init_serial(sp).await;
+
         loop {
-            info!("led off!");
-            control.gpio_set(0, false).await;
-            Timer::after(Duration::from_secs(1)).await;
+            if let Some(payload) = read_telegram(&mut han_reader).await {
+                let message = to_string::<Payload, 1024>(&payload).unwrap();
+                send_message(client, message.as_bytes()).await
+            }
 
-            info!("led on!");
             control.gpio_set(0, true).await;
-            count = count + 1;
+            Timer::after(Duration::from_millis(50)).await;
 
-
-            let payload = Payload { data: count };
-
-            let message = to_string::<Payload, 1024>(&payload).unwrap();
-
-            send_message(&mut client, message.as_bytes()).await;
+            control.gpio_set(0, false).await;
         }
     }
 }
--- src/serial.rs
+++ src/serial.rs
@@ -1,12 +1,11 @@
-use defmt::{error, info, unwrap};
-use embassy_executor::Spawner;
+use defmt::{error, info};
 use embassy_rp::peripherals::{PIN_9, UART1};
 use embassy_rp::uart;
 use embassy_rp::uart::BufferedUartRx;
-use han::{AsyncReader, Error};
+use han::{AsyncReader, Direction, Error, Line, Object, Power, Telegram};
 use static_cell::StaticCell;
 
-use crate::Irqs;
+use crate::{Irqs, Payload};
 
 pub struct SerialPeripherals {
     rx_pin: PIN_9,
@@ -19,39 +18,78 @@ impl SerialPeripherals {
     }
 }
 
-#[embassy_executor::task]
-async fn serial_task(reader: &'static mut AsyncReader<BufferedUartRx<'_, UART1>>) -> ! {
-    loop {
-        match reader.next_readout().await {
-            Ok(o) => {
-                match o {
-                    None => info!("No HAN readout"),
-                    Some(r) => {
-                        match r.to_telegram() {
-                            Ok(t) => info!("Got telegram: {=str}", t.identification),
-                            Err(e) => match e {
-                                Error::InvalidFormat => error!("InvalidFormat"),
-                                Error::Checksum => error!("Checksum"),
-                                Error::UnrecognizedReference => error!("UnrecognizedReference"),
-                            }
-                        }
-                    }
-                }
-            }
-            Err(e) => error!("Read error: {:?}", e),
-        };
-    }
-}
-
 pub async fn init_serial(
-    spawner: Spawner,
     p: SerialPeripherals,
-) {
-    static RX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
-    let rx_buf = &mut RX_BUF.init([0; 16])[..];
+) -> &'static mut AsyncReader<BufferedUartRx<'static, UART1>> {
+    static RX_BUF: StaticCell<[u8; 1024]> = StaticCell::new();
+    let rx_buf = &mut RX_BUF.init([0; 1024])[..];
     let rx = BufferedUartRx::new(p.uart, Irqs, p.rx_pin, rx_buf, uart::Config::default());
     static READER: StaticCell<AsyncReader<BufferedUartRx<UART1>>> = StaticCell::new();
-    let reader= &mut *READER.init(AsyncReader::new(rx));
-    unwrap!(spawner.spawn(serial_task(reader)));
+    let reader = &mut *READER.init(AsyncReader::new(rx));
+
+    return reader;
 }
 
+pub async fn read_telegram<'a>(
+    reader: &'a mut AsyncReader<BufferedUartRx<'_, UART1>>,
+) -> Option<Payload> {
+    let readout = reader.next_readout().await.map_err(|e| {
+        error!("Read error: {:?}", e);
+        e
+    }).ok()??;
+
+    let telegram = readout.to_telegram().map_err(|e| match e {
+        Error::InvalidFormat => error!("InvalidFormat"),
+        Error::Checksum => error!("Checksum"),
+        Error::UnrecognizedReference => error!("UnrecognizedReference"),
+    }).ok()?;
+
+    info!("Got telegram: {=str}", telegram.identification);
+    return Some(build_payload(telegram));
+}
+
+fn build_payload(telegram: Telegram) -> Payload {
+    let mut payload = Payload::default();
+    telegram.objects().map(|r| r.unwrap()).for_each(|o| {
+        match o {
+            Object::DateTime(dt) => payload.time = dt,
+            Object::Energy(p, d, v) => match (p, d) {
+                (Power::Reactive, Direction::FromGrid) => payload.energy.from_grid.reactive_varh = v,
+                (Power::Reactive, Direction::ToGrid) => payload.energy.to_grid.reactive_varh = v,
+                (Power::Active, Direction::FromGrid) => payload.energy.from_grid.active_wh = v,
+                (Power::Active, Direction::ToGrid) => payload.energy.to_grid.active_wh = v,
+            }
+            Object::TotalPower(p, d, v) => match (p, d) {
+                (Power::Reactive, Direction::FromGrid) => payload.total_power.from_grid.reactive_var = v,
+                (Power::Reactive, Direction::ToGrid) => payload.total_power.to_grid.reactive_var = v,
+                (Power::Active, Direction::FromGrid) => payload.total_power.from_grid.active_w = v,
+                (Power::Active, Direction::ToGrid) => payload.total_power.to_grid.active_w = v,
+            }
+            Object::Power(l, p, d, v) => match (l, p, d) {
+                (Line::L1, Power::Reactive, Direction::FromGrid) => payload.power_l1.from_grid.reactive_var = v,
+                (Line::L1, Power::Reactive, Direction::ToGrid) => payload.power_l1.to_grid.reactive_var = v,
+                (Line::L1, Power::Active, Direction::FromGrid) => payload.power_l1.from_grid.active_w = v,
+                (Line::L1, Power::Active, Direction::ToGrid) => payload.power_l1.to_grid.active_w = v,
+                (Line::L2, Power::Reactive, Direction::FromGrid) => payload.power_l2.from_grid.reactive_var = v,
+                (Line::L2, Power::Reactive, Direction::ToGrid) => payload.power_l2.to_grid.reactive_var = v,
+                (Line::L2, Power::Active, Direction::FromGrid) => payload.power_l2.from_grid.active_w = v,
+                (Line::L2, Power::Active, Direction::ToGrid) => payload.power_l2.to_grid.active_w = v,
+                (Line::L3, Power::Reactive, Direction::FromGrid) => payload.power_l3.from_grid.reactive_var = v,
+                (Line::L3, Power::Reactive, Direction::ToGrid) => payload.power_l3.to_grid.reactive_var = v,
+                (Line::L3, Power::Active, Direction::FromGrid) => payload.power_l3.from_grid.active_w = v,
+                (Line::L3, Power::Active, Direction::ToGrid) => payload.power_l3.to_grid.active_w = v,
+            }
+            Object::Voltage(l, v) => match l {
+                Line::L1 => payload.voltage_l1_dv = v,
+                Line::L2 => payload.voltage_l2_dv = v,
+                Line::L3 => payload.voltage_l3_dv = v,
+            }
+            Object::Current(l, v) => match l {
+                Line::L1 => payload.current_l1_da = v,
+                Line::L2 => payload.current_l2_da = v,
+                Line::L3 => payload.current_l2_da = v,
+            }
+        }
+    });
+    return payload;
+}
